32cbee18c09fddbd52bbb53304f93cf2a050db47
mildrethlorena <lorenapapamija2001@gmail.com>
2021-12-16 00:20:02 -0500

    Calculos en javascript

---
+++ javascript/js/ejercicio_5.js
@@ -0,0 +1,20 @@
+"use strict"
+var num1 = 535;
+var num2 = 239; 
+/*Ahora pasamos a las funciones */
+
+function mostrarSuma(){
+    alert(num1 + num2);
+};
+function mostrarResta(){
+    alert(num1-num2);
+};
+function mostrarDivision(){
+    alert(num1/num2);
+};
+function mostrarMultiplicacion(){
+    alert(num1*num2);
+};
+function mostrarValores(){
+    alert("el primer numero es: "+ num1 + " y el segundo es: " +num2);
+};
