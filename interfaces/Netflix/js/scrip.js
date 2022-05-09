251fbf9f666b31152d1624101b105e016703a44a
mildrethlorena <lorenapapamija2001@gmail.com>
2022-05-09 11:41:19 -0500

    netflix

---
+++ interfaces/Netflix/js/scrip.js
@@ -0,0 +1,12 @@
+/* function mostrarMensajeDoctor(){
+    alert("TE GUSTA LA PELICULA DOCTOR STRANGE EN EL MULTIVERSO DE LA LOCURA?");
+
+function intercambiarLaCiudadPerdida() {
+    alert("Ya te viste intercambiar La Ciudad Perdida");
+}
+function mostrarSonic() {
+    document.getElementById("imgPrincipal").src="img/portada_sonic.jpg";
+} */
+function intercambiarImagen(ruta) {
+    document.getElementById("imgPrincipal").src=ruta;
+}
